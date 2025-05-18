5ef88536146d6ae92073fd6caa58bf2fdcc0606c
Berkant6262 <Berkant6247_@outlook.com>
2025-05-19 01:29:28 +0200

    Erstellung einer Datenbank

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    print("Hallo Welt! Das ist dein Starter-Template.")
+import sqlite3
+import hashlib
+import cryptography
+
+#Erstellung der Datenbank
+def create_db():
+    _db = sqlite3.connect("Passwörter.db")
+    _cursor = _db.cursor()
+    _cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Passwoerter (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Site TEXT NOT NULL,
+            Login TEXT,
+            Passwort TEXT
+        )
+    """)
+    _db.commit()
+    return _db
 
 if __name__ == "__main__":
-    main()
+    _db = create_db()
